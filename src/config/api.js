7da3aa91cfc79f9ed71587bd3b68c3f553c7c829
tulasiprasadk <tulasiprasadk@gmail.com>
2025-12-29 21:29:12 +0530

    fix : api issues

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-export const API_BASE = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE = import.meta.env.VITE_API_BASE;
 
 if (!API_BASE) {
-  throw new Error("❌ VITE_API_BASE_URL is not defined");
+  throw new Error("❌ VITE_API_BASE is not defined");
 }
