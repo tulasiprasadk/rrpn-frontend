c585402788ad8f62f464805d3e51dad1ecef6606
tulasiprasadk <tulasiprasadk@gmail.com>
2025-12-30 10:11:11 +0530

    error fix

--- src/products/ProductDetail.jsx
+++ src/products/ProductDetail.jsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { API_BASE } from '../api/client';
+import { API_BASE } from '../config/api';
 
 export default function ProductDetail() {
   const { id } = useParams();
